0e631d5d97574ed5372d16b2d7e985a27ddaab7d
Rendelle Bertes <118584640+zekiro-ox@users.noreply.github.com>
2024-06-05 21:37:00 +0800

    Modified

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -84,12 +84,12 @@ const ContactUsPage = () => {
               <div className="flex-shrink-0 flex items-center">
                 <img
                   className="block lg:hidden h-8 w-auto"
-                  src="Customer\src\assets\EasyTix.svg"
+                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
                   alt="EasyTix"
                 />
                 <img
                   className="hidden lg:block h-8 w-auto"
-                  src="Customer\src\assets\EasyTix.svg"
+                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
                   alt="EasyTix"
                 />
               </div>
--- src/Event.jsx
+++ src/Event.jsx
@@ -36,12 +36,12 @@ const EventsPage = () => {
               <div className="flex-shrink-0 flex items-center">
                 <img
                   className="block lg:hidden h-8 w-auto"
-                  src="Customer\src\assets\EasyTix.svg"
+                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
                   alt="EasyTix"
                 />
                 <img
                   className="hidden lg:block h-8 w-auto"
-                  src="Customer\src\assets\EasyTix.svg"
+                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
                   alt="EasyTix"
                 />
               </div>
